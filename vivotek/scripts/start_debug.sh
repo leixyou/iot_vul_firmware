b940cc381f2bb249596c212ee6f1d5072520dd7c
leixyou <leixyou@gmail.com>
2019-05-20 22:03:58 +0800

    Update start_debug.sh

--- vivotek/scripts/start_debug.sh
+++ vivotek/scripts/start_debug.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 pid=`ps|grep -v grep|grep httpd|awk '{print $1}'`
-if [ ! $pid]
-then
-/usr/sbin/httpd
-pid=`ps|grep -v grep|grep httpd|awk '{print $1}'`
+if [ ! $pid ];then
+  /usr/sbin/httpd
+  pid=`ps|grep -v grep|grep httpd|awk '{print $1}'`
 fi
 gdbserver --attach :1234 $pid
